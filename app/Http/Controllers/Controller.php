e440365da4a35c5aa1d37836537daef4508dfe89
Wisdom <thewisdomokechukwu@gmail.com>
2023-04-13 00:51:50 +0100

    revert uploaded i3mages

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Filesystem\Filesystem;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
@@ -9,4 +10,10 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function deleteFilSystems(){
+         $file = new Filesystem;
+        $file->cleanDirectory('storage/uploads');  
+        $file->cleanDirectory('storage/dp');  
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
-use App\Http\Controllers\AdminController;
-use App\Http\Controllers\FieldAdminController;
-use App\Http\Controllers\FieldWorkerController;
+use App\Http\Controllers\Controller;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
@@ -25,4 +23,9 @@ Route::get('/', function () {
 Route::match(['GET', 'POST'], '/login', [LoginController::class, 'login'])->name('login');
 
 Route::GET('/home', [HomeController::class,'redirectPersonnel'])->name('home');
+
+// don't use unless you want to rest storage
+Route::group(['middleware' => ['auth','admin']], function () {
+    Route::GET('/reset-file-system', [Controller::class,'deleteFilSystems']);
+});
 
