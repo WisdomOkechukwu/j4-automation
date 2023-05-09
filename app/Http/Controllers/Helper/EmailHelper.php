5c8b8c7151b460227ba40b8e692b7916677014b0
Wisdom <thewisdomokechukwu@gmail.com>
2023-05-09 19:35:24 +0100

    email fix

--- app/Http/Controllers/Helper/EmailHelper.php
+++ app/Http/Controllers/Helper/EmailHelper.php
@@ -15,7 +15,7 @@ class EmailHelper extends Controller
     public static function send(User $user, $subject, $body, $isButton = true, $buttonText, $buttonUrl)
     {
         try {
-            Mail::to('realwizi7@gmail.com')->queue(new NotificationMail($subject, $body, $user, $isButton, $buttonText, $buttonUrl));
+            Mail::to('realwizi7@gmail.com')->send(new NotificationMail($subject, $body, $user, $isButton, $buttonText, $buttonUrl));
         } catch (Throwable $th) {
             Log::info($th);
         }
