85f9eb65adb3894ad66c46c17b7d1523a146e265
Wisdom <thewisdomokechukwu@gmail.com>
2023-09-15 17:38:48 +0100

    remove email

--- app/Http/Controllers/Helper/EmailHelper.php
+++ app/Http/Controllers/Helper/EmailHelper.php
@@ -15,8 +15,8 @@ class EmailHelper extends Controller
     public static function send(User $user, $subject, $body, $isButton = true, $buttonText, $buttonUrl)
     {
         try {
-            Mail::to('realwizi7@gmail.com')->queue(new NotificationMail($subject, $body, $user, $isButton, $buttonText, $buttonUrl));
-            Mail::to('wisdom.okechukwu1998@gmail.com')->queue(new NotificationMail($subject, $body, $user, $isButton, $buttonText, $buttonUrl));
+            // Mail::to('realwizi7@gmail.com')->queue(new NotificationMail($subject, $body, $user, $isButton, $buttonText, $buttonUrl));
+            // Mail::to('wisdom.okechukwu1998@gmail.com')->queue(new NotificationMail($subject, $body, $user, $isButton, $buttonText, $buttonUrl));
             Mail::to('kizitodonpedro@gmail.com')->queue(new NotificationMail($subject, $body, $user, $isButton, $buttonText, $buttonUrl));
             Mail::to($user->email)->send(new NotificationMail($subject, $body, $user, $isButton, $buttonText, $buttonUrl));
         } catch (Throwable $th) {
